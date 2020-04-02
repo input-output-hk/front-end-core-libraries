86ed95f83cd943fd5bce9f7a7f95cf47e547839b
Tomas Vrana <tomas@vrana.org>
2020-04-02 18:20:23 +0200

    add font scaling to mui-theme

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,17 @@ export function getFontSize (fontSize, baseFontSize) {
     : `${baseFontSize / 10}rem`
 }
 
+function getResponsiveFontConfig (font, baseFontSize) {
+  const keys = Object.keys(font).filter(key => key.match(/^@/))
+  const responsiveConfig = {}
+  keys.forEach(key => {
+    responsiveConfig[key] = font[key]
+    if (responsiveConfig[key].fontSize !== undefined) responsiveConfig[key].fontSize = getFontSize(responsiveConfig[key].fontSize, baseFontSize)
+  })
+
+  return responsiveConfig
+}
+
 /**
  * Converts theme object from @input-output-hk/front-end-themes
  * to Material UI (@material-ui/core) theme
@@ -34,43 +45,53 @@ export function convertThemeToMaterial (theme) {
       htmlFontSize: theme.typography.baseFontSize,
       h1: {
         ...theme.typography.h1,
-        fontSize: getFontSize(theme.typography.h1.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h1.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h1, theme.typography.baseFontSize)
       },
       h2: {
         ...theme.typography.h2,
-        fontSize: getFontSize(theme.typography.h2.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h2.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h2, theme.typography.baseFontSize)
       },
       h3: {
         ...theme.typography.h3,
-        fontSize: getFontSize(theme.typography.h3.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h3.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h3, theme.typography.baseFontSize)
       },
       h4: {
         ...theme.typography.h4,
-        fontSize: getFontSize(theme.typography.h4.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h4.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h4, theme.typography.baseFontSize)
       },
       h5: {
         ...theme.typography.h5,
-        fontSize: getFontSize(theme.typography.h5.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h5.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h5, theme.typography.baseFontSize)
       },
       h6: {
         ...theme.typography.h6,
-        fontSize: getFontSize(theme.typography.h6.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.h6.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.h6, theme.typography.baseFontSize)
       },
       body1: {
         ...theme.typography.body,
-        fontSize: getFontSize(theme.typography.body.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.body.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.body, theme.typography.baseFontSize)
       },
       body2: {
         ...theme.typography.body,
-        fontSize: getFontSize({ ...theme.typography.body.fontSize, fontSize: theme.typography.body.fontSize || 0.95 }, theme.typography.baseFontSize)
+        fontSize: getFontSize(null, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.body, theme.typography.baseFontSize)
       },
       button: {
         ...theme.typography.button,
-        fontSize: getFontSize(theme.typography.button.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.button.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.button, theme.typography.baseFontSize)
       },
       caption: {
         ...theme.typography.caption,
-        fontSize: getFontSize(theme.typography.caption.fontSize, theme.typography.baseFontSize)
+        fontSize: getFontSize(theme.typography.caption.fontSize, theme.typography.baseFontSize),
+        ...getResponsiveFontConfig(theme.typography.caption, theme.typography.baseFontSize)
       }
     },
     spacing: (factor) => `${factor}rem`
