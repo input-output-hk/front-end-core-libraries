6bfa449934a1f1c4dd317c4ca7c0c3c407ab98ea
Tomas Vrana <tomas@vrana.org>
2020-05-26 20:25:51 +0200

    fix shape undefined, added shadow key

--- src/theme.js
+++ src/theme.js
@@ -47,8 +47,9 @@ export function convertThemeToMaterial (theme) {
         }
       }
     },
+    shadows: (theme.shadows) ? theme.shadows : ['none'],
     shape: {
-      borderRadius: theme.shape.borderRadius
+      borderRadius: (theme.shape && theme.shape.borderRadius) ? theme.shape.borderRadius : 0
     },
     palette: {
       ...theme.colors,
